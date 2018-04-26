b9e77e8993098379fb6f7b8d225d01ed315bd003
Daksha Patel <dakshapatel100@gmail.com>
2018-04-26 02:44:53 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,17 @@
 
 
-def fizzbuzz(int)
-  if int % 3 == 0
-   "Fizz"
-  if int % 5 == 0
-   "Buzz"
-  if int % 3 == 0 || int % 5 == 0
-   "FizzBuzz"
+def fizzbuzz(i)
+  if i % 3 == 0 && i % 5 == 0
+      # Checks if integer is divisible by both 3 & 5
+      puts "FizzBuzz"
+    elsif i % 3 == 0
+      # Checks if integer is divisible by only 3
+      puts "Fizz"
+    elsif i % 5 == 0
+      # Checks if integer is divisible by only 5
+      puts "Buzz"
+    else
+      # Otherwise prints the number itself
+      puts i
+    end
   end
-end
