feace61eaf65386d57fcd7ecff02ff28f7e41e30
Daksha Patel <dakshapatel100@gmail.com>
2018-04-26 02:41:17 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,20 +1,17 @@
 
 
-describe "fizzbuzz" do
-  it 'returns "Fizz" when the number is divisible by 3' do
-    fizz_3 = fizzbuzz(3)
+def fizzbuzz(int)
+  if int % 3 == 0
+  puts "Fizz"
 
-    expect(fizz_3).to eq("Fizz")
-  end
-
-elsif condition
+else condition
    int % 5 == 0
   puts "Buzz"
-  fizz_5 = fizzbuzz(5)
 
-elsif condition
+
+else condition
    int % 3 == 0 || int % 5 == 0
   puts "FizzBuzz"
-   expect(fizz_15).to eq("FizzBuzz"
+
   end
 end
