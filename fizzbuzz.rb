fb399a88307d2c9680c70040dd46ffb12c5a5eee
Daksha Patel <dakshapatel100@gmail.com>
2018-04-26 02:42:41 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,14 +1,11 @@
 
 
 def fizzbuzz(int)
-  if
-    int % 3 == 0
+  if int % 3 == 0
   puts "Fizz"
-else
-   int % 5 == 0
+  if int % 5 == 0
   puts "Buzz"
-else
-   int % 3 == 0 || int % 5 == 0
+  if int % 3 == 0 || int % 5 == 0
   puts "FizzBuzz"
   end
 end
