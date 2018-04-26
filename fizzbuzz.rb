b091f1d1607de319ad20391f7a6bf3d846e061e7
Daksha Patel <dakshapatel100@gmail.com>
2018-04-26 02:35:40 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,12 +2,12 @@
 
 def fizzbuzz(int)
   if int % 3 == 0
-    "Fizz"
+  puts "Fizz"
   end
 if int % 5 == 0
-   "Buzz"
+  puts "Buzz"
   end
 if int % 3 == 0 || int % 5 == 0
-    "FizzBuzz"
+  puts "FizzBuzz"
   end
 end
