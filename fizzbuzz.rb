c0ca474572c379857125e4bd342ee72fe84c39f8
Daksha Patel <dakshapatel100@gmail.com>
2018-04-26 02:38:04 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,13 +3,16 @@
 def fizzbuzz(int)
   if int % 3 == 0
   puts "Fizz"
+  expect(fizz_3).to eq("Fizz")
 
 elsif condition
    int % 5 == 0
   puts "Buzz"
+  fizz_5 = fizzbuzz(5)
 
 elsif condition
    int % 3 == 0 || int % 5 == 0
   puts "FizzBuzz"
+   expect(fizz_15).to eq("FizzBuzz"
   end
 end
